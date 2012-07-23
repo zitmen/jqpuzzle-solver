12c5c61a3fdf87364c399298a939668191fc1ca0
Martin Ovesny <zitmen@gmail.com>
2012-07-24 00:23:12 +0200

    C# solver empty project [init]

---
+++ csharp-console-solver/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace csharp_console_solver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+        }
+    }
+}
