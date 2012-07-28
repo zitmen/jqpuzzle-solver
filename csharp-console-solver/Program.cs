73f456c64e0f146b720787d4d4da6b8b4a8b46ad
Martin Ovesny <zitmen@gmail.com>
2012-07-28 19:48:31 +0200

    IDA*: remembers the last step; H: Manhattan distance+linear conflicts heuristic.

--- csharp-console-solver/Program.cs
+++ csharp-console-solver/Program.cs
@@ -3,6 +3,10 @@ using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Collections;
 
+// TODO: optimalizace - nahradit alokace a nahradit path=null za path.clear
+// TODO: Pattern DB
+// TODO: Disjoint Pattern DBs
+
 namespace HelloWorld
 {
     class Hello 
@@ -39,8 +43,9 @@ namespace HelloWorld
             //
             // Compute the solution
             Solution solution = IDAStar(new Board(board));
-            // TODO: test Board class methods
-            //       nekam do DFS nutno vrazit MoveBackwards!!! (navrat z rekurze)
+            solution.PrintPath();
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
         }
 
         // Algorithm IDA*
@@ -50,8 +55,11 @@ namespace HelloWorld
             while (true)
             {
                 solution = DLS(0, board, solution);
-                if (solution.path != null)
+                if (solution.solved == true)
                     return solution;
+                else if (solution.cost == int.MaxValue)
+                    return null;
+                solution.cost += 1; // celkem += 2, protoze v DLS se inkrementuje o 1
             }
         }
 
@@ -61,15 +69,16 @@ namespace HelloWorld
         {
             int minimum_cost = start_cost + Heuristic(board);
             if (minimum_cost > solution.cost) return new Solution(null, minimum_cost);
-            if (board.IsGoal()) return solution;
+            if (board.IsGoal()) { solution.solved = true; return solution; }
             //
             int next_cost_limit = int.MaxValue;
-            foreach (Direction m in board.Successors())
+            foreach (Direction m in board.Successors(solution.path))
             {
                 int new_start_cost = start_cost + board.MoveCost(m);
-                Solution new_solution = DLS(new_start_cost, board, new Solution(MoveTowards(board, solution.path, m), solution.cost));
-                if (new_solution.path != null)
-                    return new_solution;
+                solution.path = MoveTowards(board, solution.path, m);
+                Solution new_solution = DLS(new_start_cost, board, new Solution(solution.path, solution.cost));
+                if (new_solution.solved == true) return new_solution;
+                solution.path = MoveBackwards(board, solution.path);
                 next_cost_limit = Math.Min(next_cost_limit, new_solution.cost);
             }
             return new Solution(null, next_cost_limit);
@@ -81,25 +90,71 @@ namespace HelloWorld
             int h = 0;
             //
             // Misplaced tiles
+            /*
             for (int i = 0; i < board.Height; i++)
-                for (int j = 0; j < board.Width; i++)
+                for (int j = 0; j < board.Width; j++)
                     if (board.Tiles[i, j] != ((i * board.Width) + (j + 1)))
                         h++;
+            */
             //
             // Manhattan distance
-            /*
+            int row, col;
             for (int i = 0; i < board.Height; i++)
-                for (int j = 0; j < board.Width; i++)
-                    h += ???;
-            */
-            //
-            // Manhattan distance + linear colisions
-            /*
+            {
+                for (int j = 0; j < board.Width; j++)
+                {
+                    row = (board.Tiles[i, j] - 1) / board.Width;
+                    col = (board.Tiles[i, j] - 1) % board.Width;
+                    h += Math.Abs(row - i) + Math.Abs(col - j);
+                }
+            }
+            // + linear conflicts
+            //   --> in rows
             for (int i = 0; i < board.Height; i++)
-                for (int j = 0; j < board.Width; i++)
-                    h += ???;
-            */
+            {
+                for (int j = 0; j < board.Width; j++)
+                {
+                    row = (board.Tiles[i, j] - 1) / board.Width;
+                    if (row == i)   // is the tile in it's correct row?
+                    {
+                        // is there any [i,k] tile from the same row that needs to go before the [i,j] tile?
+                        col = (board.Tiles[i, j] - 1) % board.Width;
+                        for (int k = j + 1; k < board.Width; k++)
+                        {
+                            if(((board.Tiles[i, k] - 1) / board.Width) == i)    // the same row?
+                            {
+                                if (((board.Tiles[i, k] - 1) % board.Width) < col)    // before the [i,j] tile?
+                                    h += 2;
+                            }
+                        }
+                    }
+                }
+            }
+            //   --> in columns
+            for (int j = 0; j < board.Width; j++)
+            {
+                for (int i = 0; i < board.Height; i++)
+                {
+                    col = (board.Tiles[i, j] - 1) % board.Width; 
+                    if (col == j)   // is the tile in it's correct column?
+                    {
+                        // is there any [k,j] tile from the same column that needs to go before the [i,j] tile?
+                        row = (board.Tiles[i, j] - 1) / board.Width;
+                        for (int k = i + 1; k < board.Height; k++)
+                        {
+                            if (((board.Tiles[k, j] - 1) % board.Width) == j)    // the same column?
+                            {
+                                if (((board.Tiles[k, j] - 1) / board.Width) < row)    // before the [k,j] tile?
+                                    h += 2;
+                            }
+                        }
+                    }
+                }
+            }
             //
+            // Pattern Database
+            //
+            // Disjoint Pattern Database
             return h;
         }
 
@@ -112,10 +167,11 @@ namespace HelloWorld
             return path;
         }
 
-        static List<Direction> MoveBackwards(Board board, List<Direction> path, Direction move)  // Rollback
+        static List<Direction> MoveBackwards(Board board, List<Direction> path)  // Rollback
         {
+            Direction[] reverse_direction = { Direction.NONE, Direction.RIGHT, Direction.LEFT, Direction.DOWN, Direction.UP };
             if (path == null) return null;
-            board.Move(path[path.Count - 1]);
+            board.Move(reverse_direction[(int)path[path.Count - 1]]);
             path.RemoveAt(path.Count - 1);
             return path;
         }
@@ -124,16 +180,27 @@ namespace HelloWorld
         {
             public int cost { get; set; }
             public List<Direction> path { get; set; }
+            public bool solved { get; set; }
 
             public Solution(List<Direction> path, int cost)
             {
                 this.cost = cost;
                 this.path = path;
+                this.solved = false;
+            }
+
+            public void PrintPath()
+            {
+                Console.Write("Solution path ({0:d}): ", path.Count);
+                foreach (Direction d in path)
+                    Console.Write(d.ToString() + ",");
+                Console.WriteLine();
             }
         }
 
         enum Direction
         {
+            NONE,
             LEFT,
             RIGHT,
             UP,
@@ -245,20 +312,21 @@ namespace HelloWorld
             public bool IsGoal()
             {
                 for (int i = 0; i < height; i++)
-                    for (int j = 0; j < width; i++)
+                    for (int j = 0; j < width; j++)
                         if(board[i,j] != ((i * width) + (j + 1)))
                             return false;
                 //
                 return true;
             }
 
-            public List<Direction> Successors()
+            public List<Direction> Successors(List<Direction> path)
             {
+                Direction last_move = (((path != null) && (path.Count > 0)) ? path[path.Count - 1] : Direction.NONE);
                 List<Direction> moves = new List<Direction>();
-                if (CanGoLeft()) moves.Add(Direction.LEFT);
-                if (CanGoRight()) moves.Add(Direction.RIGHT);
-                if (CanGoUp()) moves.Add(Direction.UP);
-                if (CanGoDown()) moves.Add(Direction.DOWN);
+                if (CanGoLeft() && (last_move != Direction.RIGHT)) moves.Add(Direction.LEFT);
+                if (CanGoRight() && (last_move != Direction.LEFT)) moves.Add(Direction.RIGHT);
+                if (CanGoUp() && (last_move != Direction.DOWN)) moves.Add(Direction.UP);
+                if (CanGoDown() && (last_move != Direction.UP)) moves.Add(Direction.DOWN);
                 return moves;
             }
 
