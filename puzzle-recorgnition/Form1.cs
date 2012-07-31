78cb68f6ad786012ad997f210eba585f867bad25
Martin Ovesny <zitmen@gmail.com>
2012-07-31 17:58:56 +0200

    EMGU CV Puzzle recognition project init.

---
+++ puzzle-recorgnition/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+
+namespace puzzle_recorgnition
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Openfile = new OpenFileDialog();
+            if (Openfile.ShowDialog() == DialogResult.OK)
+            {
+                Image<Bgr, Byte> My_Image = new Image<Bgr, byte>(Openfile.FileName);
+                pictureBox1.Image = My_Image.ToBitmap();
+            }
+        }
+    }
+}
