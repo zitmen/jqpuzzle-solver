8b70e11ea87d58c1dd72d83608e615495441f264
Martin Ovesny <zitmen@gmail.com>
2012-08-01 03:06:03 +0200

    Recognizer: basic GUI + SURF detector.

--- puzzle-recorgnition/Form1.Designer.cs
+++ puzzle-recorgnition/Form1.Designer.cs
@@ -28,40 +28,156 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(13, 13);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(368, 23);
+            this.button1.Size = new System.Drawing.Size(172, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Open an image...";
+            this.button1.Text = "Open image of a starting board...";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
             this.pictureBox1.Location = new System.Drawing.Point(13, 43);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(368, 350);
+            this.pictureBox1.Size = new System.Drawing.Size(303, 303);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(241, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Learn tiles";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(598, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Label tiles";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(370, 43);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(303, 303);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(370, 11);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(193, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Open image of a shuffled board...";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(712, 236);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Result:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(715, 252);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(112, 94);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(712, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Board dimensions:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(715, 60);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(42, 17);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.Text = "3x3";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(715, 84);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(42, 17);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "4x4";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(393, 405);
+            this.ClientSize = new System.Drawing.Size(839, 370);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Puzzle recognition";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +185,15 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
     }
 }
 
--- puzzle-recorgnition/Form1.cs
+++ puzzle-recorgnition/Form1.cs
@@ -7,17 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-using Emgu.CV;
 using Emgu.Util;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Features2D;
 using Emgu.CV.Structure;
 
 namespace puzzle_recorgnition
 {
     public partial class Form1 : Form
     {
+        int board_size;
+        Image<Bgr, Byte> original, shuffled;
+
         public Form1()
         {
             InitializeComponent();
+            //
+            original = new Image<Bgr, byte>("input.png");
+            pictureBox1.Image = original.ToBitmap();
+            //
+            shuffled = new Image<Bgr, byte>("puzzle.png");
+            pictureBox2.Image = shuffled.ToBitmap();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            board_size = 3;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            board_size = 4;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -25,9 +47,71 @@ namespace puzzle_recorgnition
             OpenFileDialog Openfile = new OpenFileDialog();
             if (Openfile.ShowDialog() == DialogResult.OK)
             {
-                Image<Bgr, Byte> My_Image = new Image<Bgr, byte>(Openfile.FileName);
-                pictureBox1.Image = My_Image.ToBitmap();
+                original = new Image<Bgr, byte>(Openfile.FileName);
+                pictureBox1.Image = original.ToBitmap();
             }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Openfile = new OpenFileDialog();
+            if (Openfile.ShowDialog() == DialogResult.OK)
+            {
+                shuffled = new Image<Bgr, byte>(Openfile.FileName);
+                pictureBox2.Image = shuffled.ToBitmap();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 1. aplikuj mrizku board_size*board_size dlazdic
+            // 2. SURF na kazdou dlazdici a klice uloz do ArrayListu [0-9] nebo [0-15]
+            //original.Draw(new Rectangle(0, 0, 10, 10), new Bgr(Color.Red), 2);    // <-- draw grid!!
+            // subregion: Image<TColor, TDepth> GetSubRect(Rectangle rect);
+            // SURF: foreach subregion a nebo rozdelit featury do subregionu...co je lepsi...
+            SURFDetector surf = new SURFDetector(500, false);
+            Image<Gray, byte> original_gray = original.Convert<Gray, byte>();
+            MKeyPoint[] keypoints = surf.DetectKeyPoints(original_gray);    // DetectFeatures? ten to udela rovnou...jakej je rozdil?
+            ImageFeature[] features = surf.ComputeDescriptors(original_gray, keypoints);
+            foreach(ImageFeature feature in features)
+                original.Draw(new CircleF(feature.KeyPoint.Point, 3.0f), new Bgr(Color.Red), 1);
+            //
+            pictureBox1.Image = original.ToBitmap();
+            pictureBox1.Invalidate();
+            //
+            /*
+            BruteForceMatcher<float> matcher = new BruteForceMatcher<float>(DistanceType.L2);
+            matcher.Add(modelDescriptors);
+            
+            indices = new Matrix<int>(observedDescriptors.Rows, k);
+
+            using (Matrix<float> dist = new Matrix<float>(observedDescriptors.Rows, k))
+            {
+                matcher.KnnMatch(observedDescriptors, indices, dist, k, null);
+                mask = new Matrix<byte>(dist.Rows, 1);
+                mask.SetValue(255);
+                Features2DToolbox.VoteForUniqueness(dist, uniquenessThreshold, mask);
+            }
+
+            int nonZeroCount = CvInvoke.cvCountNonZero(mask);
+            if (nonZeroCount >= 4)
+            {
+                nonZeroCount = Features2DToolbox.VoteForSizeAndOrientation(modelKeyPoints, observedKeyPoints, indices, mask, 1.5, 20);
+                if (nonZeroCount >= 4)
+                    homography = Features2DToolbox.GetHomographyMatrixFromMatchedFeatures(modelKeyPoints, observedKeyPoints, indices, mask, 2);
+            }
+            */
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // 1. SURF na shuffled
+            // 2. match naucenych dlazdic na obrazek a z pozic vytvorit poradi (result)
+            // 3. Write result: textBox1 = "";
+            MCvFont font = new MCvFont(FONT.CV_FONT_HERSHEY_COMPLEX, 1.0, 1.0);
+            shuffled.Draw("1", ref font, new Point(50, 50), new Bgr(Color.Red));
+            pictureBox2.Image = shuffled.ToBitmap();
+            pictureBox2.Invalidate();
         }
     }
 }
